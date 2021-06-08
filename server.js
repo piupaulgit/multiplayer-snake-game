0e8de648be09ae8ef3d3c74d6f34e8aeda7ba631
piupaulgit <contactpiu619@gmail.com>
2021-06-08 12:11:45 +0530

    rooms created

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const io = require('socket.io')({
-    cors: {
-      origin: '*',
-    }
-});
+
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { instrument } = require("@socket.io/admin-ui");
+const httpServer = createServer();
 const { makeid } = require('./utils');
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
+
+const clientRooms = {}
 
 io.on('connection', client => {
     client.emit('init', {data: 'hello'})
@@ -15,19 +24,37 @@ io.on('connection', client => {
 
     // functions
     function handleNewGame(){
-      const roomName = makeid(6);
+      // create unique roomName/gameCode
+      const roomName = makeid(5);
+
+      // send this game code to server
       client.emit('gameCode', roomName);
-      console.log(io.sockets)
+
+      // assign the gamecode as clientId
+      clientRooms[client.id] = roomName;
+
+      // user join the room after creating room
+      client.join(roomName);
+
+      // player one
+      client.number = 1;
+
+      // send player info to server
+      client.emit('init', 1)
     }
 
     function handleJoinGame(roomNumber){
-      const room = io.sockets.adapter.rooms['FWJ_VLPLXt1WzDzwAAAC'];
-      console.log(room, roomNumber)
+      const room = io.sockets.adapter.rooms.has(roomNumber);
+      console.log(room,'rrom')
     }
 
     function handleKeyDown(keyCode){
       // console.log(keyCode, 'keeeeyyy')
     }
 })
+
+instrument(io, {
+  auth: false
+});
 
 io.listen(3300)
